e6e44447f559f51e7f2123eb3e232cd4f57591a9
Artun Çolak <artuncolak97@gmail.com>
2021-05-23 13:32:46 +0300

    Profil sayfası eklendi

---
+++ frontend/src/pages/Profile.jsx
@@ -0,0 +1,35 @@
+import { Card, Col, Row } from "react-bootstrap";
+import { BookOpen, Heart, Save } from "react-feather";
+import Post from "../components/Post";
+import ProfileCard from "../components/profile/ProfileCard";
+
+export default function Profile() {
+  return (
+    <div>
+      <ProfileCard />
+      <Row className="mt-3">
+        <Col className="mb-3" sm>
+          <Card>
+            <Card.Body className="d-flex flex-column" style={{ gap: 20 }}>
+              {/* TODO: Link olarak değiştirilecek */}
+              <div>
+                <BookOpen /> Paylaşımlar
+              </div>
+              <div>
+                <Heart /> Beğenilenler
+              </div>
+              <div>
+                <Save /> Kaydedilenler
+              </div>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col sm>
+          <Post data={{ title: "Başlık", body: "Deneme" }} />
+          <Post data={{ title: "Başlık", body: "Deneme" }} />
+          <Post data={{ title: "Başlık", body: "Deneme" }} />
+        </Col>
+      </Row>
+    </div>
+  );
+}
