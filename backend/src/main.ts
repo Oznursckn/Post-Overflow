2227fb0289db3f04b87db546b369dcd7febda377
Artun Çolak <artuncolak97@gmail.com>
2021-05-29 00:20:05 +0300

    Dependencyler güncellendi swagger konfigurasyonu apiDocs klasörüne aktarıldı

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,11 +3,11 @@ import express from "express";
 import dotenv from "dotenv";
 import colors from "colors";
 import cookieParser from "cookie-parser";
-import swaggerUi from "swagger-ui-express";
-import swaggerDoc from "./config/swaggerConfig.json";
+import compression from "compression";
 
 import logger from "./middlewares/logger";
 import { connectToDatabase } from "./config/database";
+import apiDocs from "./config/apiDocs";
 import controllers from "./controllers";
 
 async function start() {
@@ -17,11 +17,12 @@ async function start() {
   dotenv.config();
   colors.enable();
 
+  app.use(compression());
   app.use(logger);
   app.use(cookieParser());
   app.use(express.json());
   app.use("/api", controllers);
-  app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(swaggerDoc));
+  app.use("/api/docs", apiDocs);
 
   console.clear();
   console.log(`${`[Server]`.green} Environment: ${process.env.NODE_ENV}`);
