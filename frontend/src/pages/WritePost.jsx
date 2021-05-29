3039dd9c1516172098b24116b6626e8c0be44ea8
Merve Sezer <sezermerve2259@gmail.com>
2021-05-29 14:55:59 +0300

    post yazma sayfası eklendi

--- frontend/src/pages/WritePost.jsx
+++ frontend/src/pages/WritePost.jsx
@@ -1,21 +1,30 @@
-import { Button,Card } from "react-bootstrap";
+import { Button, Card } from "react-bootstrap";
 import React from "react";
 
 export default function WritePost() {
   return (
     <div>
-      <Card className="text-center">
-        <Card.Header>Featured</Card.Header>
+      <Card>
+        <h1>
+          <Card.Header className="text-center">Featured</Card.Header>
+        </h1>
         <Card.Body>
-          <Card.Title>Special title treatment</Card.Title>
-          <Card.Text>
-            With supporting text below as a natural lead-in to additional
-            content.
-          </Card.Text>
-          <Button variant="primary">Yayınla</Button>
-          <Button variant="success">Kaydet</Button>
+          <h6>
+            <Card.Text>
+              With supporting text below as a natural lead-in to additional
+              content.
+              With supporting text below as a natural lead-in to additional
+              content. With supporting text below as a natural lead-in to additional
+              content. With supporting text below as a natural lead-in to additional
+            </Card.Text>
+          </h6>
         </Card.Body>
-        <Card.Footer className="text-muted">2 days ago</Card.Footer>
+        <Card.Footer className="text-muted text-center">
+          <Button variant="primary">Yayınla</Button>
+          <Button className="ml-3" variant="success">
+            Kaydet
+          </Button>
+        </Card.Footer>
       </Card>
     </div>
   );
